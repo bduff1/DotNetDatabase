c8268b8603d3b6517b2469491eae9a4cd0e15cec
bduff1 <48037581+bduff1@users.noreply.github.com>
2019-09-30 19:04:36 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,119 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NLog;
+//using NLog;
 using System.IO;
+using TicketingSystemFile;
+using Ticketing_With_Classes;
 
-namespace Movie_Library_BD
+namespace TicketingSystemFile
 {
     class Program
     {
-        string file { get; set; }
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            string filepath = "..\\..\\movies.csv";
+            string file = "ticketData.txt";
+            string choice;
+
+            //NewTicketFile ticketList = new NewTicketFile();
+
+            TicketFile ticketList = new TicketFile();
+
+            ticketList.FilePath = file;
+
+            ticketList.LoadFile();
 
 
-            DisplayMenu(true);
-            string userSelection = Console.ReadLine();
 
 
-            MovieFile movieList = new MovieFile();
-
-            movieList.FilePath = filepath;
-
-            movieList.LoadFile();
-
-            do
-            {
-                if (userSelection == "1")
-                {
-                    // display from movies.csv file
-                    int recordCount = 0;
-
-
-                    foreach (var movie in movieList.Movies)
-                    {
-                        recordCount++;
-
-
-                        Console.WriteLine("Title: {0}", movie.Title);
-                        movie.DisplayGenres();
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine();
-
-                }
-                else if (userSelection == "2")
-                {
-                    // add a movie, if duplicated, throw exception
-                    Console.WriteLine("Enter a title");
-                    string movieTitle = Console.ReadLine();
-
-                    
-                    string genreName = "";
-                    string genre = "";
-                    do
-                    {
-                        Console.WriteLine("Enter a Genre, enter done when finished");
-                        genre = Console.ReadLine();
-                        genreName += genre + "|";
-
-                    } while (genre.ToLower() != "done");
-                    genreName = genreName.Substring(0);
-
-                    bool duplicate = false;
-                    // check for duplicate title
-                    using (StreamReader sr = new StreamReader(filepath))
-                    {
-                        while (!sr.EndOfStream && !duplicate)
-                        {
-                            string line = sr.ReadLine();
-
-                            string[] values = line.Split(',');
-                            if (values[1] == movieTitle)
-                            {
-                                
-                                duplicate = true;
-                                throw new Exception("You entered a duplicate item");
-                            }
-                        }
-                    }
-                    if (!duplicate)
-                    {
-                        // write to the file
-                        using (StreamWriter sw = new StreamWriter(filepath, true))
-                        {
-                         
-                            int movieID = 500000;
-                            sw.WriteLine("{0},{1},{2}", movieID, movieTitle, genreName);
-                        }
-                    }
-                }
-                else if (userSelection == "3")
-                {
-                    // end the application
-                }
-             
-
-               DisplayMenu(false);
-               userSelection = Console.ReadLine();
-
-            } while (userSelection != "3");
-
-
-            Console.ReadLine();
-
-        }
-
-        public static void DisplayMenu(bool firstTime)
-        {
             
-            Console.WriteLine("1) Display Movies");
-            Console.WriteLine("2) Add a movie");
-            Console.WriteLine("3) Exit application");
-        }
+            {
+                // ask user a question
+                Console.WriteLine("1) Read data from file.");
+                Console.WriteLine("2) Create file from data.");
+                Console.WriteLine("Enter any other key to exit.");
+                // input response
+                choice = Console.ReadLine();
 
+
+
+                if (choice == "1")
+                {
+                    if (File.Exists(file))
+                    {
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("File does not exist");
+                    }
+
+                }
+                else if (choice == "2")
+                {
+                  
+
+
+                    Console.WriteLine("Enter a ticket");
+
+                    //using(StreamReader sr = new StreamReader(file))
+                    //{
+                    //    string line = sr.ReadLine();
+
+                    //    string[] values = line.Split(',');
+
+
+                    //}
+
+                    using (StreamWriter sw = new StreamWriter(file, true))
+                    {
+                        string TicketID = "";
+                        string SummaryIssue = "";
+                        string Status = "";
+                        string Priority = "";
+                        string Submitter = "";
+                        string Assigned = "";
+                        string Watching = "";
+
+                        sw.WriteLine("{0},{1},{2},{3},{4},{5}", TicketID, SummaryIssue, Status, Priority, Submitter, Assigned, Watching);
+
+                        sw.Close();
+                        Console.ReadLine();
+                    }
+                } while (choice == "1" || choice == "2") ;
+
+            }
+        }
     }
 }
+
